1f6cdecc285cd2dc8f875dcf8c826acb0fa88005
Amarasinghe <dilan@dilan-amarasinghes-macbook-pro.local>
2011-04-08 15:18:50 -0500

    main() updated with useful code

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 
 int main (int argc, char const *argv[])
 {
-	printf("Hello World\n");
+	int a;
+	int b;
+	a = 3;
+	b = 5;
+	printf("Square of %d is %d\n", a, square(a));
+	printf("Cube of %d is %d\n", a, cube(a));
 	return 0;
 }
